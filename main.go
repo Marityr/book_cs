d9ce531d172578847048ef4d48f98b77a2dab646
Voronov <dmitriy.woronow@gmail.com>
2023-08-16 20:33:47 +0300

    add

--- main.go
+++ main.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	ll := NewList[int]()
-
-	ll.PushBack(1)
-	ll.PushBack(2)
-	ll.PushBack(3)
-	ll.PushBack(4)
-	ll.PushBack(5)
-	ll.PushBack(6)
-
-	fmt.Println(ll)
+	fmt.Println(factorial(5))
 }
